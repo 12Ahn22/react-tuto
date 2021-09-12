4f0cb9309fbf8baf1743936e0a92c30ae84b8b12
ayo <12ayo22@gmail.com>
2021-09-12 21:03:14 +0900

    [feat] username으로 user검색기능

--- crud/src/components/UserList.js
+++ crud/src/components/UserList.js
@@ -3,6 +3,12 @@ import UserItem from './UserItem';
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const onChangeHandler = (e) => {
+    setSearch(e.currentTarget.value);
+  };
 
   // User정보를 jsonplaceholder에서 가져오기
   useEffect(() => {
@@ -23,12 +29,45 @@ const UserList = () => {
     fetchUser();
   }, []);
 
+  // 필터된 Users를 갱신하는 useEffect
+  useEffect(() => {
+    // 입력값이 변경될 경우, 다시 리렌더링을 한다.
+    // 입력값으로 정규표현식을 만들어 저장한다.
+    const reg = new RegExp(`^${search}`, 'gi'); // 대소문자 구분x
+    // 필터하기
+    const filtered = users.filter((user) => {
+      return reg.test(user.name);
+    });
+    setFilteredUsers(filtered);
+  }, [search, users]);
+
   return (
     <>
       {/* <UserItem users={users} /> */}
-      {users.map((user) => (
+      <div>
+        <input
+          type='text'
+          placeholder='Search Name'
+          onChange={onChangeHandler}
+          value={search}
+        />
+      </div>
+      {/* {users.map((user) => (
         <UserItem key={user.id} user={user} />
-      ))}
+      ))} */}
+      {filteredUsers.length !== 0 ? (
+        <>
+          {filteredUsers.map((user) => (
+            <UserItem key={user.id} user={user} />
+          ))}
+        </>
+      ) : (
+        <>
+          {users.map((user) => (
+            <UserItem key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </>
   );
 };
